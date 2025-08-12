35890b5f166d10d1fc3f37e6d2405631e1aeabfc
xNoNamex769 <camilocaicedo2011@gmail.com>
2025-08-12 18:51:03 -0500

    matar base de datos

--- src/server.ts
+++ src/server.ts
@@ -42,9 +42,7 @@ async function connectDB() {
       colors.blue.bold("Conexión exitosa a la Base de datos echo por Alex")
     );
 
-    await db.query("SET FOREIGN_KEY_CHECKS = 0");
-    await db.sync({ force: true });
-    await db.query("SET FOREIGN_KEY_CHECKS = 1");
+    
     try {
       const [results, metadata] = await db.query("SELECT * FROM asistencia");
       console.log("Datos de ejemplo:", results);
