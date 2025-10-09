b78b758b80c07512b385c98ee87a5bfd4713687b
xNoNamex769 <camilocaicedo2011@gmail.com>
2025-10-08 22:48:58 -0500

    correcion

--- src/server.ts
+++ src/server.ts
@@ -80,7 +80,7 @@ app.use(express.json({ limit: "10mb" }));
 app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
 app.use(express.json());
 app.use("/api/dialogflow", dialogflowRouter);
-
+app.use("/api/enviarcorreo", EmailRoutes);
 app.use("/api/actividad", ActividadRouter);
 app.use("/api/alquilerelementos", AlquilerElementosRouter);
 app.use("/api/asistencia", AsistenciaRouter);
